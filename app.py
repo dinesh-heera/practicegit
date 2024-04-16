ef83568e73003f3ff7fbb1ff74026c7bfa5146dc
Dinesh Heera <dineshheera@gmail.com>
2024-04-15 17:00:53 -0700

    substraction updated

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def addition(num1, num2,num3):
     return num1 + num2+num3
 
-def subtraction(num1, num2):
-    return num1 - num2
+def subtraction(num1, num2,num3):
+    return num1 - num2 - num3
 
 def multiplication(num1, num2):
     return num1 * num2
